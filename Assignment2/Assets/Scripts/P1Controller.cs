4740f49e71bc0368c97b0581438384d00c723c07
Tim Chong <timothy.chong99@gmail.com>
2019-10-28 23:02:50 -0700

    Added code to make player MOOOOVE
    Gonna try and go for the final door opening animation!

---
+++ Assignment2/Assets/Scripts/P1Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1Controller : MonoBehaviour
+{
+    // Initialize Variables
+    public float speed = 5;
+    public Rigidbody rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Forwards and backwards movement
+        rigid.MovePosition(
+                             transform.position + 
+                            (transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime) +
+                            (transform.right * Input.GetAxis("Horizontal")
+                            * speed * Time.deltaTime)
+                            );
+
+    }
+
+}
